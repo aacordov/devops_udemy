a720c7450c7758e4e38e938dc000c199f964d4d6
Allan Cordova <aacordov@gmail.com>
2022-10-18 00:57:23 -0500

    modificacion en la clase v2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,5 +1,6 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +12,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String meessage="default message cambio test devops";
+		String meessage= "default message cambio test devops";
 		Assert.assertNotNull(meessage);
 	}
 
